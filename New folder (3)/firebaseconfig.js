00260e2f414d14f0c5d43d4c93c0b49c726add9c
Wadood <107611585+WADOOD-UL-ISLAM@users.noreply.github.com>
2025-05-11 12:11:10 +0500

    Add files via upload

---
+++ New folder (3)/firebaseconfig.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDFeNgXbph3n32yBtjt2JwUQO82UfZK4tI",
+    authDomain: "spinnerd1-cb95e.firebaseapp.com",
+    projectId: "spinnerd1-cb95e",
+    storageBucket: "spinnerd1-cb95e.appspot.com",
+    messagingSenderId: "24610685268",
+    appId: "1:24610685268:web:6557414e56eab2a78b08c1",
+    measurementId: "G-374VF9ZLFB"
+};
